f3b2048371aee85b0fe3e6890572e5273ca17527
Aruna20042005 <arunadh29@gmail.com>
2026-05-18 00:11:10 +0530

    Fix signup and login JWT variables

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,38 +1,22 @@
 const express = require("express");
-
 const router = express.Router();
 
 const User = require("../models/User");
-
 const jwt = require("jsonwebtoken");
 
-
 // SIGNUP ROUTE
-
 router.post("/signup", async (req, res) => {
-
     try {
-
         const { username, email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
-
             return res.status(400).json({
                 message: "User already exists",
             });
-
         }
-        const token = jwt.sign(
 
-            {
-                id: newUser._id,
-            },
-
-            process.env.JWT_SECRET
-
-        );
         const newUser = new User({
             username,
             email,
@@ -41,73 +25,65 @@ router.post("/signup", async (req, res) => {
 
         await newUser.save();
 
+        const token = jwt.sign(
+            {
+                id: newUser._id,
+            },
+            process.env.JWT_SECRET
+        );
+
         res.json({
             message: "User Registered Successfully",
+            token,
         });
 
     } catch (error) {
+        console.log(error);
 
         res.status(500).json({
             message: "Error Registering User",
         });
-
     }
-
 });
 
-
 // LOGIN ROUTE
-
 router.post("/login", async (req, res) => {
-
     try {
-
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
 
         if (!user) {
-
             return res.status(404).json({
                 message: "User not found",
             });
-
         }
 
         if (user.password !== password) {
-
             return res.status(400).json({
                 message: "Invalid Password",
             });
-
         }
 
         const token = jwt.sign(
-
             {
-                id: existingUser._id,
+                id: user._id,
             },
-
             process.env.JWT_SECRET
-
         );
 
         res.json({
-
             message: "Login Successful",
-
             token,
-
         });
 
     } catch (error) {
+        console.log(error);
 
         res.status(500).json({
             message: "Login Error",
         });
-
     }
-
 });
 
 module.exports = router;
