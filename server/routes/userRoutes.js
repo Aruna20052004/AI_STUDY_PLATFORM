b8c246f25b97426ca94bb3ea714d5b46fe287ae6
Aruna20042005 <arunadh29@gmail.com>
2026-05-18 01:50:20 +0530

    Restore working user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,20 +1,28 @@
 const express = require("express");
+
 const router = express.Router();
 
 const User = require("../models/User");
+
 const jwt = require("jsonwebtoken");
 
+
 // SIGNUP ROUTE
+
 router.post("/signup", async (req, res) => {
+
     try {
+
         const { username, email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
 
         if (existingUser) {
+
             return res.status(400).json({
                 message: "User already exists",
             });
+
         }
 
         const newUser = new User({
@@ -26,10 +34,13 @@ router.post("/signup", async (req, res) => {
         await newUser.save();
 
         const token = jwt.sign(
+
             {
                 id: newUser._id,
             },
+
             process.env.JWT_SECRET
+
         );
 
         res.json({
@@ -38,52 +49,72 @@ router.post("/signup", async (req, res) => {
         });
 
     } catch (error) {
+
         console.log(error);
 
         res.status(500).json({
             message: "Error Registering User",
         });
+
     }
+
 });
 
+
 // LOGIN ROUTE
+
 router.post("/login", async (req, res) => {
+
     try {
+
         const { email, password } = req.body;
 
         const user = await User.findOne({ email });
 
         if (!user) {
+
             return res.status(404).json({
                 message: "User not found",
             });
+
         }
 
         if (user.password !== password) {
+
             return res.status(400).json({
                 message: "Invalid Password",
             });
+
         }
 
         const token = jwt.sign(
+
             {
                 id: user._id,
             },
+
             process.env.JWT_SECRET
+
         );
 
         res.json({
+
             message: "Login Successful",
+
             token,
+
         });
 
     } catch (error) {
+
         console.log(error);
 
         res.status(500).json({
             message: "Login Error",
         });
+
     }
+
 });
 
 module.exports = router;
